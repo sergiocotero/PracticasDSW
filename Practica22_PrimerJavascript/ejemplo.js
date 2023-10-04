2084cace3bf87a5b1aa43d296d3b6be99b23533e
sergiocotero <sergio.cotero@uabc.edu.mx>
2023-10-03 18:13:55 -0700

    Hicimos pruebas con javascript por primera vez, definimos variables, imprimimos arrays y por ultimo una SUMA

---
+++ Practica22_PrimerJavascript/ejemplo.js
@@ -0,0 +1,32 @@
+//alert("Hola mundo");
+
+/*var numeroUno = 1
+var numeroDos = 2
+var numeroTres = 3
+var resultado = numeroUno+numeroDos+numeroTres
+
+alert(resultado)*/
+
+/*var mensaje = "Bienvenido al mi sirio web";
+var texto1 = "Texto entre comillas dobles";
+var texto2 = 'Texto entre comillas simples';
+
+alert(mensaje+"\n"+texto1+"\n"+texto2);*/
+
+/*var clienteRegistrado = false;
+var ivaIncluido = true;
+
+alert(ivaIncluido);*/
+
+/*var vertor = ['Tijuana', 'Mexicali', 'Rosarito', 'Ensenada', 'Tecate'];
+
+alert(vertor[0]);*/
+
+var num1 = prompt("Ingrese primer número");
+var num2 = prompt("Ingrese segundo número");
+var resSuma = parseFloat(num1)+parseFloat(num2);
+var resResta = parseFloat(num1)-parseFloat(num2);
+var resMulti = parseFloat(num1)*parseFloat(num2);
+var resDivi = parseFloat(num1)/parseFloat(num2);
+
+alert("Suma: " +resSuma+ "\n" +"Resta: " +resResta+ "\n" +"Multiplicación: " +resMulti+ "\n" +"División: "+resDivi);
