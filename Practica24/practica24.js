6afe8126cfb332caa62bc98432fd6400b04c1dc4
sergiocotero <sergio.cotero@uabc.edu.mx>
2023-10-09 19:30:04 -0700

    Es la práctica de condiciones con puros if, de la práctica de Estructuras de control

---
+++ Practica24/practica24.js
@@ -0,0 +1,12 @@
+var num1 = 5;
+var num2 = 8;
+
+if(num1 <= num2){
+    alert("El número 1 no es mayor que el número 2");
+}
+if(num2 <= 0){
+    alert("Número 2 es positivo");
+}
+if(++num1 < num2){
+    alert("Incrementar uno en el número 1, no lo hace mayor que el número 2");
+}
